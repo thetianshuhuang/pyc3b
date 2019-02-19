7a35154aae2e0fa273529295c05cc460b466b9be
thetianshuhuang <thetianshuhuang@gmail.com>
2019-02-19 12:15:16 -0600

    Added opcodes

--- parse.py
+++ parse.py
@@ -1,40 +1,55 @@
 
 import re
+import collections
+
+ifmt = collections.namedtuple('ifmt', ['argtype', 'width', 'signed'])
+
+
+DR = ifmt(argtype='REG')
+SR1 = ifmt(argtype='REG')
+SR2 = ifmt(argtype='REG')
+op_spec = ifmt(argtype='')
+PCoffset9 = ifmt(argtype='LABEL', width=9, signed=True)
+PCoffset11 = ifmt(argtype='LABEL', width=11, signed=True)
+offset6 = ifmt(argtype='IMM', width=6, signed=True)
+amount4 = ifmt(argtype='IMM', width=4, signed=False)
+trapvec8 = ifmt(argtype='IMM', width=8, signed=True, force_hex=True)
+fill = ifmt(argtype='IMM', width=16, signed=None)
 
 
 class Instruction:
 
     OPCODES = {
-        "ADD": [0x1000, 'DR', 'SR1', 'op.spec'],
-        "AND": [0x5000, 'DR', 'SR1', 'op.spec'],
-        "BR": None,
-        "BRN": None,
-        "BRZ": None,
-        "BRP": None,
-        "BRNZ": None,
-        "BRNP": None,
-        "BRZP": None,
-        "BRNZP": None,
-        "JMP": None,
-        "JSR": None,
-        "JSRR": None,
-        "LDB": None,
-        "LDW": None,
-        "LEA": None,
-        "NOT": None,
-        "RET": None,
-        "RTI": None,
-        "LSHF": None,
-        "RSHFL": None,
-        "RSHFA": None,
-        "STB": None,
-        "STW": None,
-        "TRAP": None,
-        "XOR": None,
-        "HALT": None,
-        "NOP": None,
-        ".FILL": None,
-        ".ORIG": None,
+        "ADD": [0x1000, DR, SR1, op_spec],
+        "AND": [0x5000, DR, SR1, op_spec],
+        "BR": [0x0E00, PCoffset9],
+        "BRN": [0x0800, PCoffset9],
+        "BRZ": [0x0400, PCoffset9],
+        "BRP": [0x0200, PCoffset9],
+        "BRNZ": [0x0C00, PCoffset9],
+        "BRNP": [0x0A00, PCoffset9],
+        "BRZP": [0x0600, PCoffset9],
+        "BRNZP": [0x0E00, PCoffset9],
+        "JMP": [0xC000, SR1],
+        "JSR": [0x4800, PCoffset11],
+        "JSRR": [0x4000, SR1],
+        "LDB": [0x2000, DR, SR1, offset6],
+        "LDW": [0x6000, DR, SR1, offset6],
+        "LEA": [0xE000, SR1, PCoffset9],
+        "NOT": [0x903F, DR, SR1],
+        "RET": [0xC1C0],
+        "RTI": [0x8000],
+        "LSHF": [0xD000, DR, SR1, amount4],
+        "RSHFL": [0xD010, DR, SR1, amount4],
+        "RSHFA": [0xD030, DR, SR1, amount4],
+        "STB": [0x3000, DR, SR1, offset6],
+        "STW": [0x7000, DR, SR1, offset6],
+        "TRAP": [0xF000, trapvec8],
+        "XOR": [0x9000, DR, SR1, op_spec],
+        "HALT": [0xF025],
+        "NOP": [0x0000],
+        ".FILL": [fill],
+        ".ORIG": [fill],
     }
 
     ILLEGAL_LABELS = {
@@ -57,6 +72,10 @@ class Instruction:
         self.label = args[0] if len(args) > 0 else None
         self.opcode = args[1] if len(args) > 1 else None
         self.args = args[2:]
+
+    def assemble(self):
+
+        self.OPCODES[self.opcode]
 
     def assign(self, addr, table):
         if self.label is not None:
