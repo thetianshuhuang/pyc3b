b0cd1ebb5b131605d02c7592bcfc03a744e1d1f3
Tianshu Huang <thetianshuhuang@gmail.com>
2019-03-12 16:47:05 -0500

    Added header and made show hint default instead of run all

--- tester.py
+++ tester.py
@@ -1,10 +1,19 @@
-"""
+r"""
      _    ___ _____      _____       _
     | |  / __|__ / |__  |_   _|__ __| |_ ___ _ _
     | |_| (__ |_ \ '_ \   | |/ -_|_-<  _/ -_) '_|
     |____\___|___/_.__/   |_|\___/__/\__\___|_|
     UT Austin EE460N LC3b Clock-Level Simulator Test Engine
     Tianshu Huang, Spring 2019
+
+Usage : python tester.py [-flags]
+
+Flags
+-----
+-c / --csv
+    Use CSV output instead (echos to stdout -- must pipe)
+--alu, --cc, --memory, --jmp, --all
+    Only run the specified test case
 """
 
 import sys
@@ -16,6 +25,27 @@ _EXEC_TARGET = "a.out"
 _UCODE_FILE = "ucode3"
 _TEST_CASES = ['alu', 'jmp', 'memory', 'cc']
 _OBJ_TMP_FILE = "__tmp__.obj"
+
+__HEADER = p.render(r"""
+     _    ___ _____      _____       _
+    | |  / __|__ / |__  |_   _|__ __| |_ ___ _ _
+    | |_| (__ |_ \ '_ \   | |/ -_|_-<  _/ -_) '_|
+    |____\___|___/_.__/   |_|\___/__/\__\___|_|
+    UT Austin EE460N LC3b Clock-Level Simulator Test Engine
+    Tianshu Huang, Spring 2019
+""", p.BR + p.RED, p.BOLD)
+
+__HINT = """
+Usage : python tester.py [-flags]
+
+Flags
+-----
+-c / --csv
+    Use CSV output instead (echos to stdout -- must pipe)
+--alu, --cc, --memory, --jmp
+    Only run the specified test case
+
+"""
 
 
 def test(e):
@@ -35,10 +65,21 @@ def test(e):
 
 if __name__ == '__main__':
 
+    # Don't print header if CSV output is selected
+    if not (p.argparse.is_flag('csv') or p.argparse.is_flag('c')):
+        p.print(__HEADER)
+
+    # Run all tests
+    if p.argparse.is_flag('all'):
+        for evil in _TEST_CASES:
+            test(evil)
+        exit(0)
+
+    # Run single test
     for evil in _TEST_CASES:
         if p.argparse.is_flag(evil):
             test(evil)
             exit(0)
 
-    for evil in _TEST_CASES:
-        test(evil)
+    # not exited yet -> no test specified
+    p.print(__HINT)
